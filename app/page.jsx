1211c00a7803ffd3972e9f9fdfb138af25d7327a
Joey ter Meulen <joeytermeulen@gmail.com>
2024-06-30 14:43:39 +0200

    chore: add a header component with navigation

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,18 @@
-// import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./page.module.css";
+import LogoImg from "@/public/logo.png";
 
 export default function HomePage() {
   return (
     <main >
       <h1>Welcome to ScubApp</h1>
       <p>Your favorite scuba dive log!</p>
+
+      {/* <Image src={LogoImg} alt="ScubApp logo" /> */}
+
+      <Link href="/auth">Login</Link>
     </main>
   );
 }
---
+++ components/Header.jsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import styles from "./Header.module.css";
+import LogoImg from "@/public/logo.png";
+
+export default function Header() {
+  return (
+    <>
+      <div className={styles.wave}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <defs>
+            <linearGradient id="waveGradient" x1="0%" y1="0%" x2="0%" y2="100%">
+              <stop offset="0%" stopColor="rgba(255,255,255,0.3)" />
+              <stop offset="100%" stopColor="rgba(255,255,255,0)" />
+            </linearGradient>
+          </defs>
+          <path
+            fill="url(#waveGradient)"
+            d="M0,192L60,160C120,128,240,64,360,53.3C480,43,600,85,720,133.3C840,181,960,235,1080,224C1200,213,1320,139,1380,101.3L1440,64L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+          ></path>
+          <path
+            fill="url(#waveGradient)"
+            d="M0,192L60,197.3C120,203,240,213,360,229.3C480,245,600,267,720,234.7C840,203,960,117,1080,96C1200,75,1320,117,1380,138.7L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
+
+      <header className={styles.header}>
+
+        <Link href="/" className={styles.logo}>
+          <Image src={LogoImg} alt="ScubApp logo" />
+          <h1>ScubApp</h1>
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul>
+            {/* TODO: nav links should be different for logged in state */}
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link href="/dives">My dives</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
