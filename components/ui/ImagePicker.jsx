84bbe4ecac9f126e316716c6fe593983af09d635
Joey ter Meulen <joeytermeulen@gmail.com>
2024-07-08 23:49:10 +0200

    feat: add image preview to imagepicker component

--- components/ui/ImagePicker.jsx
+++ components/ui/ImagePicker.jsx
@@ -1,6 +1,30 @@
-import styles from "./Input.module.css";
+"use client";
+
+import { useState } from "react";
+
+import styles from "./ImagePicker.module.css";
+import Image from "next/image";
 
 export default function Input({ label, name, ...rest }) {
+  const [imageFile, setImageFile] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      setImageFile(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setImageFile(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  };
+
   return (
     <div className={styles.inputWrapper}>
       <label htmlFor={name} className={styles.label}>
@@ -11,8 +35,14 @@ export default function Input({ label, name, ...rest }) {
         accept="image/png, image/jpeg, image/jpg, image/heic, image/heif"
         name={name}
         className={styles.input}
+        onChange={handleImageChange}
         {...rest}
       ></input>
+      {imageFile && (
+        <div className={styles.imagePreview}>
+          <Image src={imageFile} alt="User selected image" fill />
+        </div>
+      )}
     </div>
   );
 }
