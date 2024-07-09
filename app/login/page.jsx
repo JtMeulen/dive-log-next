1cc0ae59c88eda22d3131486dc5d8e0adea4b149
Joey ter Meulen <joeytermeulen@gmail.com>
2024-07-09 23:17:40 +0200

    feat: implement authentication

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,19 +1,45 @@
 "use client";
 
+import { useRef } from "react";
 import Link from "next/link";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 import styles from "./page.module.css";
 
 import Input from "@/components/ui/Input";
 import Button from "@/components/ui/Button";
-import { loginAction } from "@/lib/actions/login";
 
 export default function LoginPage() {
+  const router = useRouter();
+  const formRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const res = await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirect: false,
+    });
+
+    if (res?.error) {
+      console.log("Error:", res.error);
+    }
+
+    if (res?.ok) {
+      formRef.current?.reset();
+      router.push("/dives");
+    }
+  };
+
   return (
     <main className={styles.main}>
       <h1>Login</h1>
 
-      <form className={styles.form} action={loginAction}>
+      <form className={styles.form} onSubmit={handleSubmit} ref={formRef}>
         <Input type="email" name="email" label="Email" required />
         <Input type="password" name="password" label="Password" required />
         <Button type="submit">Login</Button>
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { useRef } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 import styles from "./page.module.css";
 
@@ -7,11 +11,32 @@ import Button from "@/components/ui/Button";
 import { signupAction } from "@/lib/actions/signup";
 
 export default function SignupPage() {
+  const formRef = useRef();
+  const router = useRouter();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const response = await signupAction(formData);
+
+    if (response?.error) {
+      // TODO: Show error message to user
+      console.log(response.error);
+    }
+
+    if (response?.user) {
+      formRef.current?.reset();
+      router.push("/login");
+    }
+  };
+
   return (
     <main className={styles.main}>
       <h1>Sign up</h1>
 
-      <form className={styles.form} action={signupAction}>
+      <form className={styles.form} onSubmit={handleSignup} ref={formRef}>
         <Input type="text" name="name" label="Name" required />
         <Input type="email" name="email" label="Email" required />
         <Input type="password" name="password" label="Password" required />
