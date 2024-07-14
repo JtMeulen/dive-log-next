778da0767d66ef5de66826068fc2a22b519be038
Joey ter Meulen <joeytermeulen@gmail.com>
2024-07-14 19:02:31 +0200

    feat: add delete user functionality

--- app/profile/_components/Actions.jsx
+++ app/profile/_components/Actions.jsx
@@ -3,9 +3,10 @@
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
+import { deleteUser } from "@/lib/actions/deleteUser";
 import Button from "@/components/Button";
-import styles from "./page.module.css";
 import ButtonLink from "@/components/ButtonLink";
+import styles from "./page.module.css";
 
 export default function Actions() {
   const router = useRouter();
@@ -17,13 +18,30 @@ export default function Actions() {
     router.push("/login");
   };
 
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    
+    const response = await deleteUser();
+
+    if (response.success) {
+      signOut();
+      router.push("/");
+    } else {
+      console.log(response.error);
+    }
+  }
+
   return (
     <div className={styles.actions}>
-      <ButtonLink variant="primary" href="/profile/edit">Edit Account</ButtonLink>
+      <ButtonLink variant="primary" href="/profile/edit">
+        Edit Account
+      </ButtonLink>
       <Button variant="secondary" onClick={handleSignOut}>
         Sign Out
       </Button>
-      <Button variant="secondary">Delete Account</Button>
+      <Button variant="secondary" onClick={handleDelete}>
+        Delete Account
+      </Button>
     </div>
   );
 }
